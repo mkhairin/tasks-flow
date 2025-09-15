1ed6f3fb2dbd77599230f4cfd5b1e4ec6d1613d3
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2025-09-15 19:14:36 +0800

    Update Tag.php

--- task_flow_api/app/Models/Tag.php
+++ task_flow_api/app/Models/Tag.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'tags';
+    protected $fillable = ['user_id', 'name_tag'];
+
+    public function tasks(): BelongsToMany{
+        return $this->belongsToMany(Task::class);
+    }
 }
