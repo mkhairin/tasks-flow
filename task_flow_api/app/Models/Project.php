40c0247368a005368d90b7f12c7d381cbdd9a2c7
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2025-09-15 13:10:26 +0800

    create model

--- task_flow_api/app/Models/Project.php
+++ task_flow_api/app/Models/Project.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Project extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'projects';
+    protected $fillable = ['user_id', 'name_project', 'description'];
+
+    /**
+     * Mendefinisikan relasi: Proyek ini milik seorang User.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Mendefinisikan relasi: Proyek ini memiliki banyak Task.
+     */
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- task_flow_api/app/Models/Task.php
+++ task_flow_api/app/Models/Task.php
@@ -3,8 +3,56 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'tasks';
+    protected $fillable = [
+        'user_id',
+        'project_id',
+        'parent_id',
+        'title',
+        'description',
+        'status',
+        'priority',
+        'due_date'
+    ];
+
+    /**
+     * Relasi: Task ini milik sebuah Project.
+     */
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    /**
+     * Relasi: Task ini memiliki banyak sub-task (children).
+     */
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Task::class, 'parent_id');
+    }
+
+    /**
+     * Relasi: Task ini memiliki banyak sub-task (children).
+     */
+    public function children(): HasMany
+    {
+        return $this->hasMany(Task::class, 'parent_id');
+    }
+
+    /**
+     * Relasi: Task ini bisa memiliki banyak Tag.
+     */
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
